1a4c032e5a34700d2688923d39af9d4a03fc9824
Tim Smith <timsmith@Tims-MacBook-Air.local>
2023-06-25 07:49:45 -0500

    Added environment component to tell when I'm in dev mode

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import { Environment } from "./environment";
 
 export const Layout = ({ children }) => {
 	return (
 		<div>
+			{process.env.NODE_ENV === "development" && <Environment />}
 			<header className="header">
 				<Link href="/">Home</Link>
 				<Link href="/pages">Pages</Link>
