efca53cbdd307ee960db88c61d7587f4b1b234f5
Tim Smith <timsmith@Tims-MacBook-Air.local>
2023-07-23 07:15:47 -0500

    Modified casing and filename in Footer.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,14 +5,14 @@ import { getIcon } from "../../utils/getIcon";
 import { tinaField } from "tinacms/dist/react";
 
 export const Footer = ({ socialNav }: GlobalFooter) => (
-	<footer className={style.Footer}>
+	<footer className={style.footer}>
 		<nav>
-			<ul className={style.FooterSocials}>
+			<ul className={style.footerSocials}>
 				{socialNav &&
 					socialNav?.map((social, i: number) => (
 						<li key={`socialNav_${i}`}>
 							<Link
-								className={style.FooterSocialLink}
+								className={style.footerSocialLink}
 								href={social.url || ""}
 								aria-label={social.title || ""}
 								data-tina-field={tinaField(social)}
