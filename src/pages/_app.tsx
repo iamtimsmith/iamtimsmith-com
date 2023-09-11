e77229a7fe4a1d0d3360c767ffd98a9fdcf7aac5
Tim Smith <timsmith@Tims-MacBook-Air.local>
2023-09-11 15:16:09 -0500

    Added favicon to site.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,14 @@
+import Head from "next/head";
+import global from "../../content/global/index.json";
 import "../styles/style.css";
 
-const CustomApp = ({ Component, pageProps }) => <Component {...pageProps} />;
+const CustomApp = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <link rel="icon" href={global.header.favicon} />
+    </Head>
+    <Component {...pageProps} />
+  </>
+);
 
 export default CustomApp;
