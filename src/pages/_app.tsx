5012107bbf10096a122c9c1d9feb9298998ad1dc
Tim Smith <timsmith@Tims-MacBook-Air.local>
2023-09-12 22:02:50 -0500

    Added GA scripts to app.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,27 @@
 import Head from "next/head";
+import Script from "next/script";
 import global from "../../content/global/index.json";
 import "../styles/style.css";
+
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
 
 const CustomApp = ({ Component, pageProps }) => (
   <>
     <Head>
       <link rel="icon" href={global.header.favicon} />
     </Head>
+    <Script
+      src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      strategy="afterInteractive"
+    />
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){window.dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${GA_MEASUREMENT_ID}');
+  `}
+    </Script>
     <Component {...pageProps} />
   </>
 );
