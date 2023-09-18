f03edff51c8a9aa25a604803e7cd55b69a9a80f0
Tim Smith <timsmith@Tims-MacBook-Air.local>
2023-09-18 11:20:22 -0500

    Updated SEO component.

--- sitemap.config.js
+++ sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.WEBSITE_URL || "http://localhost:1125",
+  siteUrl: process.env.NEXT_PUBLIC_WEBSITE_URL || "http://localhost:1125",
   generateRobotsTxt: true,
   // ...other options
 };
--- src/components/Seo/Seo.tsx
+++ src/components/Seo/Seo.tsx
@@ -1,37 +1,42 @@
-import Head from "next/head";
+import { NextSeo } from "next-seo";
 import { useRouter } from "next/router";
-import { PropsWithChildren } from "react";
 import config from "../../../content/global/index.json";
 
-interface SeoProps extends PropsWithChildren {
+interface SeoProps {
   title: string;
   description: string;
   image: string;
 }
 
-export const Seo = ({ title, description, image, children }: SeoProps) => {
+export const Seo = ({ title, description, image }: SeoProps) => {
   const router = useRouter();
-  const baseUrl =
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:1125"
-      : "https://www.iamtimsmith.com";
+  const route = process.env.NEXT_PUBLIC_WEBSITE_URL + router.asPath;
 
   return (
-    <Head>
-      <title>
-        {title} | {config.header.siteName}
-      </title>
-      <meta name="description" content={description} />
-      <link rel="canonical" href={`${baseUrl}${router.asPath}`} />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@iam_timsmith" />
-      <meta name="og:title" content={title} />
-      <meta name="og:description" content={description} />
-      <meta name="og:image" content={image} />
-      <meta name="og:type" content="website" />
-      <meta name="og:url" content={`${baseUrl}${router.asPath}`} />
-      <meta name="og:site_name" content={config.header.siteName} />
-      {children}
-    </Head>
+    <NextSeo
+      title={`${title} | ${config.header.siteName}`}
+      description={description}
+      canonical={route}
+      openGraph={{
+        url: route,
+        title: title,
+        description: description,
+        images: [
+          {
+            url: image,
+            width: 800,
+            height: 600,
+            alt: title,
+            type: "image/jpeg",
+          },
+        ],
+        siteName: config.header.siteName,
+      }}
+      twitter={{
+        handle: config.author.twitter,
+        site: process.env.WEBSITE_URL,
+        cardType: "summary_large_image",
+      }}
+    />
   );
 };
