41414f2e320a00de5e03a9d58dfe71861b108f7e
Tim Smith <tgsmith489@gmail.com>
2025-01-22 10:43:53 -0600

    Added reset css and implemented.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,14 @@ import type { AppProps } from "next/app";
 import { Container } from "../components/Container";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
+import "../styles/style.css";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Container>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
-    </Container>
-  );
-}
+const CustomApp = ({ Component, pageProps }: AppProps) => (
+  <Container>
+    <Header />
+    <Component {...pageProps} />
+    <Footer />
+  </Container>
+);
+
+export default CustomApp;
