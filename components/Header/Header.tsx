f7e7f6de6c830e45533ec70f3b351306deebbb80
Tim Smith <tgsmith489@gmail.com>
2025-01-22 09:50:46 -0600

    Added Link component.

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,13 +1,60 @@
-import clsx from "clsx";
-import { FC, HTMLAttributes } from "react";
+import { FC, HTMLAttributes, useState } from "react";
+import { Link } from "../Link";
 import styles from "./styles.module.css";
 
 export interface HeaderProps extends HTMLAttributes<HTMLDivElement> {}
 
 export const Header: FC<HeaderProps> = ({ className, ...props }) => {
+  const siteName = "My Site";
+  const nav = [];
+  const [searchValue, setSearchValue] = useState("");
+
   return (
-    <div className={clsx([styles.header, className])} {...props}>
-      Header
-    </div>
+    <header className={styles.header}>
+      <Link className={styles.headerLogo} url="/">
+        {siteName}
+      </Link>
+      <div className={styles.headerItems}>
+        <nav>
+          <ul className={styles.headerNav}>
+            {nav &&
+              nav.map((item: { url: string; title: string }, i: number) => (
+                <li className={styles.headerNavItem} key={`navItem_${i}`}>
+                  <Link url={item.url}>{item?.title}</Link>
+                </li>
+              ))}
+          </ul>
+        </nav>
+        {/* <ThemeButton /> */}
+
+        {/* <Modal
+          trigger={
+            <button className={styles.headerButton} aria-label="Search">
+              <SearchIcon />
+            </button>
+          }
+        >
+          <form
+            className={styles.searchForm}
+            role="search"
+            method="get"
+            action={`${process.env.NEXT_PUBLIC_WEBSITE_URL}/search`}
+          >
+            <input
+              className={styles.searchInput}
+              type="text"
+              name="s"
+              placeholder="Search..."
+              onChange={(e) => setSearchValue(e.target.value)}
+              value={searchValue}
+              autoFocus
+            />
+            <button className={styles.searchButton} aria-label="Perform Search">
+              <SearchIcon />
+            </button>
+          </form>
+        </Modal> */}
+      </div>
+    </header>
   );
 };
