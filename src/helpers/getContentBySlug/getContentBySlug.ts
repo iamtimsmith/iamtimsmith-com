77ac6bf6ffd3e3f62e6a92c0329f10dc5f7628c9
Tim Smith <tgsmith489@gmail.com>
2025-01-31 16:51:50 -0600

    Added not found for erros with getContentBySlug

--- src/helpers/getContentBySlug/getContentBySlug.ts
+++ src/helpers/getContentBySlug/getContentBySlug.ts
@@ -17,6 +17,7 @@ export const getContentBySlug = (slug = "") => {
       slug: slug.replace("posts/", "/blog/"),
     };
   } catch (error) {
+    return notFound();
     // Handle 404 error
     if (error.code === "ENOENT") {
       return notFound();
