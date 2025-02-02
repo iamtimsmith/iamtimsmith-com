1e91ffa3939baa2da247997c8371522714e5e823
Tim Smith <tgsmith489@gmail.com>
2025-02-02 07:48:54 -0600

    Removed v3.0.0 files

--- src/components/Gif/index.ts
+++
@@ -1,1 +0,0 @@
-export { Gif, GifSchema } from "./Gif";
--- src/utils/wordCount.ts
+++
@@ -1,4 +0,0 @@
-export const wordCount = (text = "") => {
-  const wordArray = text.split(/\W/gi);
-  return wordArray.filter((word) => word).length;
-};
