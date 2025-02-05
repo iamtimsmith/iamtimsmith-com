cb3688f5e892b9cef471805935fa3f156cf587f9
Tim Smith <tgsmith489@gmail.com>
2025-02-05 11:00:28 -0600

    removed captions from gifs since there's no audio.

--- src/components/Gif/Gif.tsx
+++ src/components/Gif/Gif.tsx
@@ -1,13 +1,19 @@
+"use client";
+
 import clsx from "clsx";
-import { FC, HTMLAttributes } from "react";
+import { FC, HTMLAttributes, useEffect, useRef } from "react";
 import styles from "./styles.module.css";
 
 interface GifProps extends HTMLAttributes<HTMLDivElement> {
   src: string;
-  caption: string;
+  caption?: string;
   maxWidth?: number;
 }
 
+/**
+ * NOTES ON ACCESSIBILITY
+ * Gifs do not require captions: https://www.w3.org/WAI/media/av/captions/#checklist
+ **/
 export const Gif: FC<GifProps> = ({
   className,
   style,
@@ -15,13 +21,23 @@ export const Gif: FC<GifProps> = ({
   caption,
   maxWidth = 500,
   ...props
-}) => (
-  <figure
-    className={clsx([styles.Gif, className])}
-    style={{ ...style, maxWidth }}
-    {...props}
-  >
-    <video src={src} playsInline autoPlay muted loop />
-    {caption && <figcaption>{caption}</figcaption>}
-  </figure>
-);
+}) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    // Pause the video after 5 seconds
+    setTimeout(() => {
+      videoRef.current?.pause();
+    }, 5000);
+  });
+
+  return (
+    <figure
+      className={clsx([styles.Gif, className])}
+      style={{ ...style, maxWidth }}
+      {...props}
+    >
+      <video ref={videoRef} src={src} playsInline autoPlay muted loop />
+    </figure>
+  );
+};
