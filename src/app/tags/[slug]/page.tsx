a537f2cfbb9a4aeb6b7d8de2399942ea10772b6c
Tim Smith <tgsmith489@gmail.com>
2025-02-10 11:20:10 -0600

    Fixed tags metadata and made content more dynamic.

--- src/app/tags/[slug]/page.tsx
+++ src/app/tags/[slug]/page.tsx
@@ -1,14 +1,13 @@
 import { Container } from "../../../components/Container";
+import { Content } from "../../../components/Content";
 import { Grid } from "../../../components/Grid";
 import { Layout } from "../../../components/Layout";
+import { getContentBySlug } from "../../../helpers/getContentBySlug";
 import { getLatestPosts } from "../../../helpers/getLatestPosts";
+import { getMetadata } from "../../../helpers/getMetadata";
 
-// export const generateMetadata = ({ params }) =>
-//   getMetadata(`tags/${params.slug}`);
-
-export const metadata = {
-  title: "Tags",
-};
+export const generateMetadata = ({ params }) =>
+  getMetadata(`tags/${params.slug}`);
 
 export interface TagPageProps {
   params: Promise<{ slug: string }>;
@@ -17,14 +16,14 @@ export interface TagPageProps {
 
 const TagPage = async ({ params }: TagPageProps) => {
   const { slug } = await params;
+  const { frontmatter, content } = getContentBySlug(`tags/${slug}`);
   const posts = getLatestPosts(-1, { key: "tags", value: slug }) || [];
-  const title = slug.charAt(0).toUpperCase() + slug.slice(1);
 
   return (
     <Layout>
       <Container>
-        <h1>{title}</h1>
-        <p>A list of all the posts tagged with {slug}</p>
+        <h1>{frontmatter.title}</h1>
+        <Content>{content}</Content>
       </Container>
       <Container variant="wide">
         <Grid
