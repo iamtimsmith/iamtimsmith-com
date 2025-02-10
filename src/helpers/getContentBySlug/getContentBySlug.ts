ec0f8ab75e8beae3aa425387595bb9233d69ce12
Tim Smith <tgsmith489@gmail.com>
2025-02-10 09:58:59 -0600

    Added logging to getContentBySlug

--- src/helpers/getContentBySlug/getContentBySlug.ts
+++ src/helpers/getContentBySlug/getContentBySlug.ts
@@ -3,10 +3,13 @@ import matter from "gray-matter";
 import { notFound } from "next/navigation";
 
 export const getContentBySlug = (slug = "") => {
+  console.log("GET CONTENT BY SLUG", slug);
   if (slug.includes(".ico.mdx")) return;
 
   try {
+    console.log("GET CONTENT BY SLUG", "START TRY");
     const file = readFileSync(`content/${slug}.mdx`);
+    console.log("GET CONTENT BY SLUG FILE", file);
     const frontmatter = matter(file);
 
     return {
@@ -15,7 +18,7 @@ export const getContentBySlug = (slug = "") => {
       slug: slug.replace("posts/", "/blog/"),
     };
   } catch (error) {
-    console.log(error);
+    console.log("GET CONTENT BY SLUG FILE ERROR", error);
     return notFound();
     // Handle 404 error
     if (error.code === "ENOENT") {
